b8b4b330d3a3e020182307a0e8e03a72b5034916
JethroNatividad <natividad.jet@gmail.com>
2024-01-08 23:45:11 +0800

    done interface

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::io;
+use std::io::Write;
 // make a program that calculates the blood alchohol content, and tell if it is legal to drive or not.
 // Inputs: weight in lbs, gender, n of drinks, amount of alcohol by volume of the drinks consumed, hours since last drink.
 // Process: get total oz of alchohol in oz, r = 0.73 for men else 0.66, bac = (A × 5.14 / W × r) − .015 × H
 // Output: Your BAC is {}. /n It is not legal for you to drive.
 
 enum Gender {
-    Male: u32 = 0,
-    Female: u32 = 1
+    Male = 0,
+    Female = 1
 }
 
 fn round_decimal(number: f64, place: i32) -> f64 {
@@ -51,23 +53,34 @@ fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
 }
 
 
-def get_gender() -> Gender {
+fn get_gender() -> Gender {
     loop {
         let gender_input: u32 = get_input("What is your gender? (0 for Male, 1 for Female) ");
 
         match gender_input {
             0 => break Gender::Male,
             1 => break Gender::Female,
-            _ => println!("Invalid input. Please try again.");
+            _ => println!("Invalid input. Please try again.")
         }
     }
 }
 
 fn main() {
+    const ILLEGAL_BAC_TO_DRIVE: f64 = 0.08;
     let weight_lbs: f64 = get_input("What is your weight in lbs? ");
     let gender: Gender = get_gender();
     let n_drinks: f64 = get_input("How many drinks did you drink? ");
     let drink_alchohol_volume: f64 = get_input("How much alchohol(oz) was in per drink? ");
     let hours_since_last_drink: f64 = get_input("How many hours since your last drink? ");
+
+    let bac: f64 = calculate_bac(weight_lbs, gender, n_drinks, drink_alchohol_volume, hours_since_last_drink);
+
+    
+    match bac {
+        value if value >= ILLEGAL_BAC_TO_DRIVE => println!("Your BAC is {}.\nIt is legal for you to drive.", value),
+        value if value < ILLEGAL_BAC_TO_DRIVE => println!("Your BAC is {}.\nIt is not legal for you to drive.", value),
+        _ => println!("Something went wrong.")
+        
+    }
 
 }
