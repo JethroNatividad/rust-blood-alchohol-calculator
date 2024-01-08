68c53851bc3c45bffb035c14bc56e251189d3f8b
JethroNatividad <natividad.jet@gmail.com>
2024-01-08 20:22:17 +0800

    rename

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,14 @@ fn round_decimal(number: f64, place: i32) -> f64 {
     (number * multiplier).round() / multiplier
 }
 
-fn calculate_bac(weight: f64, gender: Gender , n_drink: f64, drink_volume: f64, hours_since_last_drink: f64) -> f64 {
-    let alchohol: f64 = n_drink * drink_volume;
+fn calculate_bac(weight: f64, gender: Gender , n_drink: f64, drink_alchohol_volume: f64, hours_since_last_drink: f64) -> f64 {
+    let alchohol: f64 = n_drink * drink_alchohol_volume;
     let alchohol_distribution_ratio: f64 = match gender {
         Gender::Male => 0.73,
         Gender:: Female => 0.66,
     };
 
-    ((alchohol * 5.14) / (weight * alchohol_distribution_ratio)) - (0.015 * hours_since_last_drink)
+    round_decimal(((alchohol * 5.14) / (weight * alchohol_distribution_ratio)) - (0.015 * hours_since_last_drink), 2)
 
 }
 
