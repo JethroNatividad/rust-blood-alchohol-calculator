3997911e07279fd9248eb460127f5b7f6e955b49
JethroNatividad <natividad.jet@gmail.com>
2024-01-08 19:22:33 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // make a program that calculates the blood alchohol content, and tell if it is legal to drive or not.
+// Inputs: weight, gender, n of drinks, amount of alcohol by volume of the drinks consumed, hours since last drink.
+// Process: get total oz of alchohol in oz, r = 0.73 for men else 0.66, bac = (A × 5.14 / W × r) − .015 × H
+// Output: Your BAC is {}. /n It is not legal for you to drive.
 
 fn main() {
     println!("Hello, world!");
