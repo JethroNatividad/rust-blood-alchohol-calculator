e713e14986249d671ac3f50892edd62eb58e008f
JethroNatividad <natividad.jet@gmail.com>
2024-01-08 20:15:53 +0800

    implement round decimal

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ enum Gender {
     Female = 1
 }
 
+fn round_decimal(number: f64, place: i32) -> f64 {
+    let multiplier: f64 = 10_f64.powi(place);
+    (number * multiplier).round() / multiplier
+}
+
 fn calculate_bac(weight: f64, gender: Gender , n_drink: f64, drink_volume: f64, hours_since_last_drink: f64) -> f64 {
     let alchohol: f64 = n_drink * drink_volume;
     let alchohol_distribution_ratio: f64 = match gender {
